efd599b057eab73d0ec471d8e375719d67f25012
kazuyoshi tlacaelel <kazu.dev@gmail.com>
2014-05-22 01:15:12 -0500

    private methods.

--- lib/borium.rb
+++ lib/borium.rb
@@ -6,38 +6,37 @@ class Borium
 
   EOT = "\004"
 
-  def self.config
-    @config ||= _resolve_configuration
+  DEFAULT_CONFIG = {
+    'connections' => 1000,
+    'host'        => 'localhost',
+    'log'         => 'STDOUT',
+    'port'        => 8200,
+    'storage'     => '/tmp/borium/'
+  }
+
+  def self.get type
+    _request "get:#{type}:"
+  end
+
+  def self.put type, job
+    _request "put:#{type}:#{job}"
+  end
+
+  def self._config
+    @_config ||= _resolve_configuration
   end
 
   def self._resolve_configuration
     configuration_file = '/etc/borium/configuration.json'
-    if File.exists?(configuration_file)
-      return JSON.parse(File.read(configuration_file))
-    else
-      return {
-        'connections' => 1000,
-        'host'        => 'localhost',
-        'log'         => 'STDOUT',
-        'port'        => 8200,
-        'storage'     => '/tmp/borium/'
-      }
-    end
+    return DEFAULT_CONFIG unless File.exists?(configuration_file)
+    JSON.parse(File.read(configuration_file))
   end
 
-  def self.get type
-    request "get:#{type}:"
-  end
-
-  def self.put type, job
-    request "put:#{type}:#{job}"
-  end
-
-  def self.request query
+  def self._request query
     counter = 0
     begin
       counter += 1
-      socket = ::TCPSocket.new config['host'], config['port']
+      socket = ::TCPSocket.new _config['host'], _config['port']
       socket.puts query + Borium::EOT
       data = ''
       while line = socket.gets
